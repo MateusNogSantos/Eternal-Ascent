1af91cd77d563549df84d5ba119bb8612c579b2f
mateus <nmateusjogos@gmail.com>
2025-08-22 22:03:18 -0300

    comentários das funções de exibição

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <GL/freeglut.h>
+using namespace std;
+
+void display(){
+
+    //Função que limpa os ruidos anteriores restantes do bits,para evitar a tela ficar piscando.
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glFlush();
+}
+
+int main(int argc,char** argv){
+
+    //Inicializa a lib com os argumentos recebidos pela função
+    glutInit(&argc, argv);
+
+    //Cria a janela e define o nome.
+    glutCreateWindow("Eternal Ascent");
+
+    //Define o tamanho e a posição da janela inicial.
+    glutInitWindowSize(3000,3000);
+    glutInitWindowPosition(0,0);
+
+    //Chama a função de exibição e faz o programa rodar em looping.
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+}
